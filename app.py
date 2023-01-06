a532f0fde29fc7cde25b5dae2a65262a783a261a
Lance <10292888+Lance1o7@users.noreply.github.com>
2023-01-05 23:55:54 -0500

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 app = Flask(__name__)
 
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     stats = os.popen("vnstat --oneline").read().split(';')
-    tx = stats[9]
-    return tx
+    tx = float(stats[9][0:-4])
+    return render_template('index.html', tx=tx, tx_p=tx/1000.0)
 
 
 if __name__ == '__main__':
